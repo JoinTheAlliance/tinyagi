d6044048980a95a766f99eb73cad987cc5ef68ea
moon <shawmakesmagic@gmail.com>
2023-07-01 00:10:37 -0700

    Update constants

--- core/constants.py
+++ core/constants.py
@@ -2,13 +2,20 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
+
 agent_name = os.getenv("AGENT_NAME")
 
 update_interval = int(os.getenv("UPDATE_INTERVAL"))
 
 default_text_model = os.getenv("DEFAULT_TEXT_MODEL")
+
 long_text_model = os.getenv("LONG_TEXT_MODEL")
+
 default_max_tokens = int(os.getenv("DEFAULT_MAX_TOKENS"))
 
-# Configure OpenAI API
 openai_api_key = os.getenv("OPENAI_API_KEY")
+
+if openai_api_key == None:
+    raise Exception(
+        "OPENAI_API_KEY environment variable not set. Please set it in a .env file."
+    )
